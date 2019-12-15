c89b9473cee892402c63df6369a9ebbc1f6dbadc
RTLiang <54389220+RTLiang@users.noreply.github.com>
2019-12-15 14:46:09 +0800

    Update and rename 回归.py to 回归线性方程.py

--- 回归线性方程.py
+++ 回归线性方程.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 win=tk.Tk()
-print('请输入x值，y值，在x值中输入 00 表示输入完毕')
+print('请输入x值，y值(至少两组数据)，在x值中输入 00 表示输入完毕')
 lx=[]
 cond=True
 sigx=0
@@ -20,15 +20,18 @@ while cond==True:
         sigxy=sigxy+x*y
         sigx2=sigx2+x*x
 n=len(lx)
-avex=sigx/n
-avey=sigy/n
-bup=sigxy-n*avex*avey
-bdown=sigx2-n*avex**2
-b0=bup/bdown
-b=str(b0)
-a=str(avey-b0*avex)
-result='回归方程为 y =' + b +' x +'+ a
-wid=len(result)
+if n <= 1:
+    result ='请输入至少两组数据'
+else :
+    avex=sigx/n
+    avey=sigy/n
+    bup=sigxy-n*avex*avey
+    bdown=sigx2-n*avex**2
+    b0=bup/bdown
+    b=str(b0)
+    a=str(avey-b0*avex)
+    result='回归方程为 y =' + b +' x +'+ a
+wid=len(result)+5
 win.title('回归线性方程')
 win.geometry('800x600')
 l=tk.Label(win,text=result,width=wid,height=7)
