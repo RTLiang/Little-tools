db09977d820641de44c8b7ba3a7237455911a0b1
RTLiang <54389220+RTLiang@users.noreply.github.com>
2020-07-31 22:18:46 +0800

    Update 数学回归线性方程.py
    解决了x,y不能输小数的bug
    添加了回归系数R^2

--- 数学回归线性方程.py
+++ 数学回归线性方程.py
@@ -1,25 +1,27 @@
-import tkinter as tk
-win=tk.Tk()
 print('请输入x值，y值(至少两组数据)，在x值中输入 00 表示输入完毕')
+ly=[]
 lx=[]
 cond=True
 sigx=0
 sigy=0
 sigxy=0
 sigx2=0
+sse = 0
+sst = 0
 while cond==True:
-    x0=input('x')
+    x0=str(input('x'))
     if x0 =='00' :
         cond=False
     else:
-        x=int(x0)
-        lx.insert(x,-1)
-        y=int(input('y'))
+        x=float(x0)
+        lx.insert(-1 , float(x))
+        y=float(input('y'))
+        ly.insert(-1, float(y))
         sigx=sigx+x
         sigy=sigy+y
         sigxy=sigxy+x*y
         sigx2=sigx2+x*x
-n=len(lx)
+n=len(ly)
 if n <= 1:
     result ='请输入至少两组数据'
 else :
@@ -31,8 +33,14 @@ else :
     b=str(b0)
     a=str(avey-b0*avex)
     result='回归方程为 y =' + b +' x +'+ a
-wid=len(result)+5
-win.title('回归线性方程')
-win.geometry('800x600')
-l=tk.Label(win,text=result,width=wid,height=7)
-l.pack()
+    print(result)
+    while n >= 1:
+        xx = float(lx[-1])
+        yy = float(ly[-1])
+        sse = float((((xx * b0) + float(a)) - avey)**2) + sse
+        sst = float((yy - avey)**2) +sst
+        del lx[-1]
+        del ly[-1]
+        n -=1
+s = sse/sst
+print('R^2=',1-s)
